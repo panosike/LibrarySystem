9b61c03e023bf8f3683585aff99d80c0134007ca
panosike <panosike@cougarnet.uh.edu>
2025-03-27 01:22:16 -0500

    3

--- sql.py
+++ sql.py
@@ -9,3 +9,32 @@ def get_connection():
         password=DB_PASSWORD,
         database=DB_NAME
     )
+    
+#Adds a new book to system 
+def create_book(title, author, year):
+    connection = get_connection()
+    cursor = connection.cursor()
+    query = "INSERT INTO books (title, author, year) VALUES (%s, %s, %s)"
+    cursor.execute(query, (title, author, year))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    
+#Gets all books
+def get_books(): 
+    connection = get_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM books")
+    books = cursor.fetchall()
+    cursor.close()
+    return books 
+
+#Update the details of the book 
+def update_book(book_id, title, author, year):
+    connection = get_connection()
+    cursor = connection.cursor()
+    query = "UPDATE books SET title = %s, author =%s, year = %s WHERE book_id = %s"
+    cursor.execute(query, (title, author, year, book_id))
+    connection.commit()
+    cursor.close()
+    connection.close()

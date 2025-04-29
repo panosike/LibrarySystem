324f7dd6fbc6fcefa04138ca9a6177c468b78185
panosike <panosike@cougarnet.uh.edu>
2025-04-28 20:00:46 -0500

    2

--- backend/credentials.py
+++ backend/credentials.py
@@ -2,3 +2,5 @@ DB_HOST = 'cis2368spring.c6xclyyzrrfa.us-east-1.rds.amazonaws.com'
 DB_USER = 'admin'
 DB_PASSWORD = '2349154DB'
 DB_NAME = 'cis2368springdb2'
+
+#I had to create a new databse hence the changed dbname compared to my initial submission on sprint 1
